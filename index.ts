7d08308e35f0c3a22ce008b4098942425a9295ab
Marie-Carrere <marie.carrere@icloud.com>
2020-10-06 12:25:12 +0200

    :sparkles: Set up server and create GET route

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+import cors from "cors";
 import express from "express";
+import { chats } from "./fixtures/db";
 
 const app = express();
 
-app.get("/_ping", (req, res) => {
-  res.send("pong");
+app.use(cors());
+
+app.get("/chats", (req, res) => {
+  res.json(chats);
 });
 
-const port = process.env.PORT || 400;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
