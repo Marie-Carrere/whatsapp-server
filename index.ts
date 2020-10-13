5e762bcbf2631e45f93a6276d5ee8c2efc06b854
Marie-Carrere <marie.carrere@icloud.com>
2020-10-13 14:15:06 +0200

    :sparkles: Set up Apollo and create GraphQL schema

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
+import { ApolloServer, gql } from "apollo-server-express";
 import cors from "cors";
 import express from "express";
 import { chats } from "./fixtures/db";
+import schema from "./schema";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/chats", (req, res) => {
   res.json(chats);
+});
+
+const server = new ApolloServer({ schema });
+
+server.applyMiddleware({
+  app,
+  path: "/graphql"
 });
 
 const port = process.env.PORT || 4000;
